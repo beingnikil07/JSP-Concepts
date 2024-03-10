7a58579b12cd7223dab96ccbfac43804b3dae11c
nikhil <kumarnikil220@gmail.com>
2024-03-10 11:07:01 +0530

    How perform designing in servlets

--- src/main/java/com/AddServlet.java
+++ src/main/java/com/AddServlet.java
@@ -15,8 +15,13 @@ public class AddServlet extends HttpServlet{
 		int b=Integer.parseInt(req.getParameter("num2"));
 		int c=a+b;
 		
-		PrintWriter out=res.getWriter();
+		PrintWriter out=res.getWriter();	
+		//How we do changes design from servlets. We are using multiple out.print() statement,That's not a good approach 
+		//To overcome this problem JSP comes into the picture with a lot of features that servlets not have 
+		out.print("<html><body bgcolor='cyan'>");
 		out.print("Output:"+c);
+		out.print("</body></html>");
+		
 		
 	}
 }
